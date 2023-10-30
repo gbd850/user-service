808412978e43bb040a8e23334e2a98eee38c3701
gbd850 <mmyszaki@gmail.com>
2023-10-30 22:30:45 +0100

    Add security config
    Add SecurityFilterChain config with whitelisted urls

--- spring-security-client/src/main/java/dev/peter/springsecurityclient/config/WebSecurityConfig.java
+++ spring-security-client/src/main/java/dev/peter/springsecurityclient/config/WebSecurityConfig.java
@@ -2,15 +2,48 @@ package dev.peter.springsecurityclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    //whitelist
+    @Bean
+    @Order(0)
+    public SecurityFilterChain whitelistFilterChain(HttpSecurity http) throws Exception {
+
+        String[] endpoints = new String[] {"/register"};
+
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher(endpoints)
+                .authorizeHttpRequests(auth -> auth.requestMatchers(endpoints).permitAll())
+                .build();
+    }
+
+    // catch-all
+    @Bean
+    @Order(1)
+    public SecurityFilterChain catchAllFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .build();
     }
 }
