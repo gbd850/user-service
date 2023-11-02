df34920e06e9682056e048527975e003520771ce
gbd850 <mmyszaki@gmail.com>
2023-11-02 16:46:29 +0100

    Add OAuth2 configuration for client

--- OAuth-authorization-server/src/main/java/dev/peter/OAuthauthorizationserver/config/DefaultSecurityConfig.java
+++ OAuth-authorization-server/src/main/java/dev/peter/OAuthauthorizationserver/config/DefaultSecurityConfig.java
@@ -1,20 +1,30 @@
 package dev.peter.OAuthauthorizationserver.config;
 
+import dev.peter.OAuthauthorizationserver.service.CustomAuthenticationProvider;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class DefaultSecurityConfig {
+
+    private CustomAuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
     }
 }
